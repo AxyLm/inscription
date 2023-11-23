69f4462200d47cdaf08285b7c8ae7dfed5968bb1
wy <moseu.yo@gmail.com>
2023-11-24 01:25:17 +0800

    feat: blockExplorer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-
 import classNames from "classnames";
+import Head from "next/head";
 import Link from "next/link";
 import { useCallback, useState } from "react";
 import {
@@ -115,7 +115,6 @@ export default function Home() {
     const timer = setInterval(async () => {
       for (const account of accounts) {
         try {
-          const baseUrl = chain.blockExplorers?.default.url;
           const hash = await client.sendTransaction({
             account,
             to: toAddress ?? account.address,
@@ -148,157 +147,169 @@ export default function Home() {
     setTimer(timer);
   }, [accounts, chain, inscription, intervalTime, rpc, toAddress]);
 
+
   return (
-    <main className=" flex flex-col items-center gap-5 py-5">
-      <h1 className=" text-5xl">Inscription</h1>
+    <>
+      <Head>
+        <title>{chain.name}</title>
+      </Head>
+      <main className=" flex flex-col items-center gap-5 py-5">
+        
+        <h1 className=" text-5xl">Inscription</h1>
 
-      <div className=" flex items-center justify-center gap-5">
-        <select
-          className=" h-10 w-[200px] rounded-lg border px-2"
-          disabled={running}
-          onChange={(e) => {
-            const text = e.target.value as ChainKey;
-            setChain(chains[text]);
-          }}
-          title="Select Chain"
-        >
-          {Object.keys(chains).map((key) => (
-            <option
-              key={key}
-              value={key}
-            >
-              {chains[key as ChainKey].name}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      <div className=" flex flex-col gap-2">
-        <span>私钥/助记词(每行一个):</span>
-        <textarea
-          className="h-[100px] w-[800px] rounded-lg border p-2"
-          placeholder="私钥/助记词"
-          disabled={running}
-          onChange={(e) => {
-            const text = e.target.value;
-            const lines = text.split("\n");
-            const accounts = lines.map((line) => {
-              try {
-                if (/^0x[a-fA-F0-9]{64}$/.test(line)) {
-                  return privateKeyToAccount(line as Hex);
-                } else {
-                  return mnemonicToAccount(line);
-                }
-              } catch (error) {
-                return undefined
-              }
-            });
-            setAccounts(accounts.filter((x) => x) as (PrivateKeyAccount | HDAccount)[]);
-          }}
-        />
-      </div>
-
-      <div className=" flex flex-col gap-2">
-        <span>to:</span>
-        <input
-          className=" h-10 w-[800px] rounded-lg border px-2"
-          placeholder="地址"
-          disabled={running}
-          onChange={(e) => {
-            const text = e.target.value;
-            isAddress(text) && setToAddress(text);
-          }}
-        />
-      </div>
-
-      <div className=" flex flex-col gap-2">
-        <span>rpc:</span>
-        <input
-          className=" h-10 w-[800px] rounded-lg border px-2"
-          placeholder="rpc"
-          disabled={running}
-          onChange={(e) => {
-            const text = e.target.value;
-            setRpc(text);
-          }}
-        />
-      </div>
-
-      <div className=" flex flex-col gap-2">
-        <span>要打的铭文（原始铭文，不是转码后的十六进制）:</span>
-        <textarea
-          className=" h-[100px] w-[800px] rounded-lg border p-2"
-          placeholder={`铭文，不要输入错了，自己多检查下，例子：\n${example}`}
-          disabled={running}
-          onChange={(e) => {
-            const text = e.target.value;
-            setInscription(text.trim());
-          }}
-        />
-      </div>
-
-      <div className=" flex items-center justify-center gap-5">
-        <button
-          className={classNames(
-            " h-10 w-[200px] rounded-full text-white transition-all hover:opacity-80",
-            running ? " bg-red-600" : " bg-green-600",
-          )}
-          onClick={() => {
-            if (!running) {
-              setRunning(true);
-              run();
-            } else {
-              setRunning(false);
-              timer && clearInterval(timer);
-            }
-          }}
-        >
-          {running ? "运行中" : "运行"}
-        </button>
-
-        <input
-          className=" h-10 w-[400px] rounded-lg border px-2"
-          placeholder="间隔时间（默认 1000ms）"
-          type="number"
-          disabled={running}
-          onChange={(e) => {
-            const text = e.target.value;
-            setIntervalTime(Number(text));
-          }}
-        />
-      </div>
-
-      <div className="mt-5 flex w-[1000px] flex-col gap-2">
-        <span>
-          {`hash count: ${logs.filter((log) => log.state === 'success').length}`}
-        </span>
-        <p>
-        </p>
-        <div className=" flex h-[600px] flex-col gap-1 overflow-auto rounded-lg bg-gray-100 px-4 py-2">
-          {logs.filter((_, i) => i <= 200).map((log, index) => (
-            <div
-              key={log.time.toString() + index}
-              className=" flex items-center"
-            >
-              <span className=" mr-2">
-                {log.time.toLocaleTimeString()}
-              </span>
-              {isSuccessLog(log) && (
-                <>
-                  <Link className=" text-blue-500 hover:underline mr-2" href={chain.blockExplorers?.default.url + '/address/' + log.owner} target="_blank">{handleAddress(log.owner)}</Link>
-                  <Link className=" text-blue-500 hover:underline mr-2" href={chain.blockExplorers?.default.url + '/address/' + log.to} target="_blank">{handleAddress(log.to)}</Link>
-                  <Link className=" text-blue-500 hover:underline" href={chain.blockExplorers?.default.url + '/tx/' + log.hash} target="_blank">{chain.name } {log.hash}</Link>
-                </>
-              )}
-              {
-                isErrorLog(log) && (
-                  <span className=" text-red-500 mr-2">{log.log}</span>
-                )
-              }
-            </div>
-          ))}
+        <div className=" flex items-center justify-center gap-5">
+          <select
+            className=" h-10 w-[200px] rounded-lg border px-2"
+            disabled={running}
+            onChange={(e) => {
+              const text = e.target.value as ChainKey;
+              setChain(chains[text]);
+            }}
+            title="Select Chain"
+          >
+            {Object.keys(chains).map((key) => (
+              <option
+                key={key}
+                value={key}
+              >
+                {chains[key as ChainKey].name}
+              </option>
+            ))}
+          </select>
+          {
+            chain.blockExplorers && <Link className=" text-blue-500 hover:underline mr-2" href={chain.blockExplorers?.default.url} target="_blank">
+              {chain.blockExplorers.default.name}
+            </Link>
+          }
         </div>
-      </div>
-    </main>
+
+        <div className=" flex flex-col gap-2">
+          <span>私钥/助记词(每行一个):</span>
+          <textarea
+            className="h-[100px] w-[800px] rounded-lg border p-2"
+            placeholder="私钥/助记词"
+            disabled={running}
+            onChange={(e) => {
+              const text = e.target.value;
+              const lines = text.split("\n");
+              const accounts = lines.map((line) => {
+                try {
+                  if (/^0x[a-fA-F0-9]{64}$/.test(line)) {
+                    return privateKeyToAccount(line as Hex);
+                  } else {
+                    return mnemonicToAccount(line);
+                  }
+                } catch (error) {
+                  return undefined
+                }
+              });
+              setAccounts(accounts.filter((x) => x) as (PrivateKeyAccount | HDAccount)[]);
+            }}
+          />
+        </div>
+
+        <div className=" flex flex-col gap-2">
+          <span>to:</span>
+          <input
+            className=" h-10 w-[800px] rounded-lg border px-2"
+            placeholder="地址"
+            disabled={running}
+            onChange={(e) => {
+              const text = e.target.value;
+              isAddress(text) && setToAddress(text);
+            }}
+          />
+        </div>
+
+        <div className=" flex flex-col gap-2">
+          <span>rpc:</span>
+          <input
+            className=" h-10 w-[800px] rounded-lg border px-2"
+            placeholder="rpc"
+            disabled={running}
+            onChange={(e) => {
+              const text = e.target.value;
+              setRpc(text);
+            }}
+          />
+        </div>
+
+        <div className=" flex flex-col gap-2">
+          <span>要打的铭文（原始铭文，不是转码后的十六进制）:</span>
+          <textarea
+            className=" h-[100px] w-[800px] rounded-lg border p-2"
+            placeholder={`铭文，不要输入错了，自己多检查下，例子：\n${example}`}
+            disabled={running}
+            onChange={(e) => {
+              const text = e.target.value;
+              setInscription(text.trim());
+            }}
+          />
+        </div>
+
+        <div className=" flex items-center justify-center gap-5">
+          <button
+            className={classNames(
+              " h-10 w-[200px] rounded-full text-white transition-all hover:opacity-80",
+              running ? " bg-red-600" : " bg-green-600",
+            )}
+            onClick={() => {
+              if (!running) {
+                setRunning(true);
+                run();
+              } else {
+                setRunning(false);
+                timer && clearInterval(timer);
+              }
+            }}
+          >
+            {running ? "运行中" : "运行"}
+          </button>
+
+          <input
+            className=" h-10 w-[400px] rounded-lg border px-2"
+            placeholder="间隔时间（默认 1000ms）"
+            type="number"
+            disabled={running}
+            onChange={(e) => {
+              const text = e.target.value;
+              setIntervalTime(Number(text));
+            }}
+          />
+        </div>
+
+        <div className="mt-5 flex w-[1000px] flex-col gap-2">
+          <span>
+            {`hash count: ${logs.filter((log) => log.state === 'success').length}`}
+          </span>
+          <p>
+          </p>
+          <div className=" flex h-[600px] flex-col gap-1 overflow-auto rounded-lg bg-gray-100 px-4 py-2">
+            {logs.filter((_, i) => i <= 200).map((log, index) => (
+              <div
+                key={log.time.toString() + index}
+                className=" flex items-center"
+              >
+                <span className=" mr-2">
+                  {log.time.toLocaleTimeString()}
+                </span>
+                {isSuccessLog(log) && (
+                  <>
+                    <Link className=" text-blue-500 hover:underline mr-2" href={chain.blockExplorers?.default.url + '/address/' + log.owner} target="_blank">{handleAddress(log.owner)}</Link>
+                    <Link className=" text-blue-500 hover:underline mr-2" href={chain.blockExplorers?.default.url + '/address/' + log.to} target="_blank">{handleAddress(log.to)}</Link>
+                    <Link className=" text-blue-500 hover:underline" href={chain.blockExplorers?.default.url + '/tx/' + log.hash} target="_blank">{chain.name } {log.hash}</Link>
+                  </>
+                )}
+                {
+                  isErrorLog(log) && (
+                    <span className=" text-red-500 mr-2">{log.log}</span>
+                  )
+                }
+              </div>
+            ))}
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
